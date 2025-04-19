eae7a2b71c237d5991c8dca853bbc1265e004d52
iamsondev <sondipkumarsk@gmail.com>
2025-04-19 13:02:31 +0600

    added Navbar and created path

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {
-  createBrowserRouter,
+ 
   RouterProvider,
 } from "react-router";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World</div>,
-  },
-]);
+import { router } from './Routes/Routes.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
